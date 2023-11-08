60b5ff2d0be0c370ae4543d9e6dd935f75bf0d8b
arafatshabbir2 <110466740+arafatshabbir2@users.noreply.github.com>
2023-11-08 21:45:34 +0600

    Fixed The Await.client Problem In vercel Not FInding Problem

--- index.js
+++ index.js
@@ -47,7 +47,6 @@ const verifyToken = (req, res, next) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
 
     const JobsCollection = client.db("Jobs").collection("jobs");
     const BidCollection = client.db("Jobs").collection("bids");
@@ -253,7 +252,6 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
